d534f8e2a3367513647b6b59e8ce098e7e73d4f7
zengcl <2402568058@qq.com>
2023-12-19 22:17:34 +0800

    集成knife4j-spring-ui

--- src/main/java/com/zengcl/controller/TestController.java
+++ src/main/java/com/zengcl/controller/TestController.java
@@ -2,10 +2,15 @@ package com.zengcl.controller;
 
 import com.zengcl.config.RedissonConfig;
 import com.zengcl.util.JedisUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.redisson.api.RLock;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import redis.clients.jedis.Jedis;
 
+import java.util.concurrent.TimeUnit;
+
+@Api(tags = "首页模块")
 @RestController
 public class TestController {
 
@@ -16,12 +21,20 @@ public class TestController {
         this.redissonConfig = redissonConfig;
         this.jedisUtil = jedisUtil;
     }
-
+    @ApiOperation(value = "向客人问好")
     @GetMapping("/a")
     public void a(){
-        Jedis jedis = jedisUtil.getJedis();
-        jedis.set("1","1");
-        String s = jedis.get("1");
-        System.out.println(s);
+        RLock lock = redissonConfig.redissonClient().getLock("a");
+        try{
+            if(!lock.tryLock(3000, TimeUnit.MILLISECONDS)){
+                System.out.println("占用");
+                return;
+            }
+            System.out.println(Thread.currentThread().getName());
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
     }
 }
